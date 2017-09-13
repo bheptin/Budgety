98a84e309bd89d21e2141097721d162fd49b73c2
Brian H <bheptin@gmail.com>
2017-09-12 22:02:20 -0400

    Data structure and budget Controller

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 	//Budget Controller
 var budgetController = (function() {
 
-	//Some code
+	var Expense = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	}
+
+	var Income = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	}
+
+	//data structure
+	var data = {
+			allItems: {
+				exp: [],
+				inc: [],
+			},
+			totals: {
+				exp: 0,
+				inc: 0
+			}
+	};
 
 })();
-
-
 
 
 
