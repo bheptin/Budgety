8512ec5dc029091b37e7f13de0b7a5b6fcc3b5bd
Brian H <bheptin@gmail.com>
2017-09-14 16:54:40 -0400

    setup for Delete

--- app.js
+++ app.js
@@ -56,10 +56,24 @@ var budgetController = (function() {
 
 			//Push it into data structure
 			data.allItems[type].push(newItem);
-
+				var ids, index;
 			//Return new Element
 			return newItem;
 		},
+
+		deleteItem: function(type, id) {
+
+			ids = data.allItems[type].map(function(current) {
+				return current.id;
+			});
+
+			index = ids.indexOf(id);
+
+			if (index !== -1) {
+				data.allItems[type].splice(index, 1)
+			}
+		},
+
 
 		calculateBudget: function() {
 
@@ -111,7 +125,8 @@ var UIController = (function() {
 		budgetLabel: '.budget__value',
 		incomeLabel: '.budget__income--value',
 		expensesLabel: '.budget__expenses--value',
-		percentageLabel: '.budget__expenses--percentage'
+		percentageLabel: '.budget__expenses--percentage',
+		container: '.container'
 	};
 
 	return {
@@ -130,13 +145,13 @@ var UIController = (function() {
 			//Create HTML string with placeholder tags
 		if (type === 'inc') {
 			element = DOMstrings.incomeContainer;
-			html = `<div class="item clearfix" id="income-%id%"><div class="item__description">%Description%</div>
+			html = `<div class="item clearfix" id="inc-%id%"><div class="item__description">%Description%</div>
 			        <div class="right clearfix"><div class="item__value">%Value%</div>
 			        <div class="item__delete"><button class="item__delete--btn"><i class="fa fa-trash" aria-hidden="true"></i></button>
 			</div></div></div>`
 		} else if (type === 'exp') {
 			element = DOMstrings.expensesContainer;
-			html = `<div class="item clearfix" id="expense-%id%"><div class="item__description">%Description%</div>
+			html = `<div class="item clearfix" id="exp-%id%"><div class="item__description">%Description%</div>
 					    <div class="right clearfix"><div class="item__value">%Value%</div><div class="item__percentage">21%</div>
 					    <div class="item__delete"><button class="item__delete--btn"><i class="fa fa-trash" aria-hidden="true"></i></button>
 					    </div></div></div>`
@@ -204,6 +219,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 			}
 
 		});
+
+		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
 	};
 
 	var updateBudget = function() {
@@ -239,7 +257,24 @@ var controller = (function(budgetCtrl, UICtrl) {
 			updateBudget();
 		}
 
+	};
 
+	var ctrlDeleteItem = function(event) {
+		var itemID, splitID, type, ID;
+		itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+		if (itemID) {
+			splitID = itemID.split('-');
+			type = splitID[0];
+			ID = parseInt(splitID[1]);
+
+			//Delete the item from the data structure
+			budgetCtrl.deleteItem(type, ID);
+
+			//Delete the item from the UI
+
+			//Update and show the new budget
+
+		}
 
 	};
 
